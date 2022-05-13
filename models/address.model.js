065fce7f0e02af0048af7a8193df6489bbc724cd
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-13 20:09:10 +0530

    Model changes

---
+++ models/address.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const addressSchema = new mongoose.Schema({
+
+    type : {
+        type : String,
+        enum : ['Point'],
+        required : true
+    },
+    coordinates : {
+        type : [Number],
+        required : true
+    }
+
+});
+
+module.exports = addressSchema;
---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const addressSchema = require('./address.model');
+
+const userSchema = new mongoose.Schema({
+
+    name : {
+        type : String,
+        required : true
+    },
+    emaildId :{ 
+        type : String,
+        required : true 
+    },
+    linkedInProfile : {
+        type : String
+    },
+    type : {
+        type : String,
+        enum : ['STUDENT', 'ADMIN'],
+        default : 'STUDENT'
+    },
+    address : {      //Embedded schema
+        type : addressSchema,
+        required : true
+    }
+
+});
+
+module.exports = userSchema;
