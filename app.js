bd9d123fa0805e91ed71ff330ffa7fc380d94781
vishal <vishalbahure22@gmail.com>
2022-05-13 20:37:31 +0530

    changes are done

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dbConfig = require("./config/db.config");
+const serverConfig = require("./config/server.config");
+
+
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/**
+ * Setup the mongodb connection and create on ADMIN user
+ */
+mongoose.connect(dbConfig.DB_URL, () => {
+    console.log("MongoDB connected");
+
+
+
+   
+
+})
+
+/**
+ * Start the express server
+ */
+app.listen(serverConfig.PORT, () => {
+    console.log("Application has started on the port ", serverConfig.PORT);
+})
